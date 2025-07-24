5e28b6ae81215f89cceb1ecfcf7dbb4d241e408c
Andrianov Oleg <ole-an@hotmail.com>
2025-07-24 16:18:13 +0300

    pick the files
    init commit with files to demo the repo

---
+++ assets/js/MainPage-D7NKE_m4.js
@@ -0,0 +1,1 @@
+import{_ as a,o as n,c as t,t as c}from"./main-HJ8FTmd_.js";const o={};function r(e,s){return n(),t("h1",null,c(e.$t("pages.main.h1")),1)}const i=a(o,[["render",r]]);export{i as default};
